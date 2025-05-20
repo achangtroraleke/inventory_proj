7215231d16a537cb3d120b96014724c6ef79f853
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2025-05-20 09:51:49 -0400

    secret key fixed.

--- container_qr/settings.py
+++ container_qr/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-
+if not SECRET_KEY:
+    raise Exception("SECRET_KEY environment variable not set!")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
